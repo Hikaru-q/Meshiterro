16881ff8cd92c2383e92a5ff45edb1a01f0be95d
Hikaru-q <132335482+Hikaru-q@users.noreply.github.com>
2023-05-15 06:17:57 +0000

    [Finish] cp-17

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  resources :users, only: [:show, :edit]
+  devise_for :users
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
   
-  
-  
-  devise_for :users
+  resources :users, only: [:show, :edit, :update]
   root to: "homes#top"
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
